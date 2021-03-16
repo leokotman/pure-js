b1665295a1b3e724675c7a612af62316244d46dd
levkotman <leoktmn@yandex.ru>
2021-03-16 20:06:59 +0100

    more items, btn 'add', simple style

--- script.js
+++ script.js
@@ -3,15 +3,48 @@ let goods = [
   {title: "Socks", price: 50},
   {title: "Jacket", price: 350},
   {title: "Shoes", price: 250},
+  {title: "Pijamas", price: 100},
+  {title: "Hat", price: 100},
+  {title: "Umbrella", price: 120},
+  {title: "Shorts", price: 130},
+  {title: "Pants", price: 200},
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+let count = 0;
+let addCount = () => {
+  return (document.querySelector(".items_count").innerHTML = count);
+};
+let addToCartButton = `<button class="add_to_cart" type="button">Add to Cart</button>`;
+
+const renderGoodsItem = (title = "Item", price = 0) => {
+  //упростил return визуально
+  let itemTitle = `<h3>${title}</h3>`;
+  let itemPrice = `<p>${price}</p>`;
+
+  return `<div class="goods_item">
+        ${itemTitle}
+        ${itemPrice}
+        ${addToCartButton}
+        </div>`;
 };
 
 const renderGoodsList = (list) => {
   let goodsList = list.map((item) => renderGoodsItem(item.title, item.price));
-  document.querySelector(".goods-list").innerHTML = goodsList;
+  document.querySelector(".goods-list").innerHTML = goodsList.join("");
+  //метод join убрал запятые между элементами получившегося массива goodList
+};
+
+//добавил возможность нажимать на кнопку добавления в корзину
+const addToCartBtnClicker = (btn) => {
+  btn.addEventListener("click", () => {
+    count++;
+    addCount();
+  });
+};
+const renderCartQuantity = () => {
+  addCount();
+  document.querySelectorAll(".add_to_cart").forEach(addToCartBtnClicker);
 };
 
 renderGoodsList(goods);
+renderCartQuantity();
