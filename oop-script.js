42612d7ad9404d0b33febc2abcf872701c275ff1
levkotman <leoktmn@yandex.ru>
2021-03-21 20:47:47 +0100

    task1 done

--- oop-script.js
+++ oop-script.js
@@ -2,14 +2,14 @@ class GoodsItem {
     constructor(title, price) {
         this.title = title;
         this.price = price;
-    }
+    };
     render() {
         return `<div class="goods_item">
         <h3>${this.title}</h3>
         <p>${this.price}</p>
         <button class="add_to_cart" type="button">Add to Cart</button>
         </div>`;
-    }
+    };
 }
 
 class GoodsList {
@@ -54,7 +54,7 @@ class GoodsList {
                 price: 200,
             },
         ];
-    }
+    };
     render() {
         let listHtml = "";
         this.goods.forEach((good) => {
@@ -62,13 +62,13 @@ class GoodsList {
             listHtml += goodItem.render();
         });
         document.querySelector(".goods-list").insertAdjacentHTML('afterbegin', listHtml);
-    }
+    };
 }
 
 class Cart {
     constructor() {
         this.count = 0
-    }
+    };
     _addCount() {
         let itemsCount = document.querySelector(".items_count");
         return itemsCount.innerHTML = this.count;
@@ -78,14 +78,39 @@ class Cart {
             this.count++;
             this._addCount();
         });
-    }
+    };
     renderCartQuantity() {
         document.querySelectorAll(".add_to_cart").forEach(this._addToCartBtnListener.bind(this));
         this._addCount();
-    }
+    };
+    //new methods
+    renderCartSum() {
+        //{item price * quantity}
+        //show sum
+    };
+    proceedToPayment() {
+        //link to the page with sum & checkout
+    };
+    cleanCart() {
+        //remove all goods, clean sum
+    };
+}
+//элемент корзины (товар в корзине)
+class ItemInCart {
+    constructor(title, price, quantity) {
+        this.title = title
+        this.price = price
+        this.quantity = quantity
+    };
+    changeQuantity() {
+        //btns + & –
+    };
+    removeFromCart() {
+        //btn 'delete item'
+    };
 }
 
-//Создание объектов и вызов методов
+//Создание объектов товаров и корзины, вызов методов
 let list = new GoodsList();
 list.fetchGoods();
 list.render();
